f65249a167883644bf4a51d2083bf48d29a79409
ajkfds <ajkfds2018@gmail.com>
2019-05-03 20:14:10 +0900

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pluginTcl
+{
+    public class Plugin : codeEditorPlugin.IPlugin
+    {
+        public void Initialize()
+        {
+            // register filetype
+//            FileTypes.CSourceFile fileType = new FileTypes.CSourceFile();
+//            codeEditor.Global.FileTypes.Add(fileType.ID, fileType);
+        }
+        public string Id { get { return "Tcl"; } }
+    }
+}
